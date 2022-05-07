630122ab4fd6948d419523aec62fc963eb81b439
Roberto Felipe <feliperoberto092@gmail.com>
2022-05-07 01:19:48 -0300

    .

--- src/home.tsx
+++ src/home.tsx
@@ -3,6 +3,19 @@ import './home.css';
 function Portfolio() {
   return (
     <div>
+      <header className='Header'>
+        <div>
+          <h1 className='NameHeader'>Roberto Felipe</h1>
+        </div>
+        <div>
+          <li className='ListaHeader'>
+            <ul>Competências</ul>
+            <ul>Projetos</ul>
+            <ul>Sobre Mim</ul>
+            <ul>Contato</ul>
+          </li>
+        </div>
+      </header>
     </div>
   );
 }
