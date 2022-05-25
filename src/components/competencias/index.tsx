ea890f65ce97d2d6bb7d1e6f88f69d6170f7b229
RobertooFelipe <feliperoberto092@gmail.com>
2022-05-25 02:43:13 -0300

    :construction:

---
+++ src/components/competencias/index.tsx
@@ -0,0 +1,13 @@
+import './style.css';
+
+
+export function Competencias(Competencia: any) {
+
+    return (
+        <div className='cardCompetencia'>
+            <p>{Competencia}</p>
+        </div>
+    )
+}
+
+
--- src/home.tsx
+++ src/home.tsx
@@ -1,13 +1,48 @@
 import { Header } from "./components/header";
 import { RobertoFelipe } from "./components/robertoFelipe";
+import { Competencias } from "./components/competencias";
+import './home.css';
 
 function Portfolio() {
+
   return (
     <div>
       <Header />
       <RobertoFelipe />
+
+      <section className="bodySection">
+
+        <div className="aboutMe">
+          <div className="titleAbout">
+            <h1>Sobre Mim</h1>
+          </div>
+
+          <article className="card">
+            <div className="circle"></div>
+            <div className="circle"></div>
+            <div className="circle"></div>
+            <div className="circle"></div>
+            <div className="card-inner">
+              <p>
+                Tenho 18 anos, sou pró-ativo assíduo, sempre motivado para o trabalho em equipe, tenho respeito, franqueza e busco sempre o conhecimento e aprimoramento técnico/profissional e de outros assuntos de meu interesse cultural, tenho um bom relacionamento em geral... Além da programação, tenho o UI/UX designer como hobby, já entreguei diversos produtos (Publicações, logos, promoções, posts de engajamento,...) desta área para clientes.
+                Já li livros como: O Poder, Mais Esperto Que o Diabo, Pai Rico Pai Pobre. Atualmente estou lendo O Poder do Hábito.
+                Tenho 18 anos, sou pró-ativo assíduo, sempre motivado para o trabalho em equipe, tenho respeito, franqueza e busco sempre o conhecimento e aprimoramento técnico/profissional e de outros assuntos de meu interesse cultural, tenho um bom relacionamento em geral... Além da programação, tenho o UI/UX designer como hobby, já entreguei diversos produtos (Publicações, logos, promoções, posts de engajamento,...) desta área para clientes.
+                Já li livros como: O Poder, Mais Esperto Que o Diabo, Pai Rico Pai Pobre. Atualmente estou lendo O Poder do Hábito.
+              </p>
+            </div>
+          </article>
+        </div>
+
+        <div className="Competencias">
+          <Competencias Competencia="comp" />
+        </div>
+
+
+      </section>
     </div>
   );
+
+
 }
 
 export default Portfolio;
