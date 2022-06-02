0c533f4339da2d705df9682b50a5d09959cc3be7
Roberto.Felipe <roberto.oliveira@anchieta.br>
2022-06-02 20:26:59 -0300

    :fire:

--- src/components/projetos/index.tsx
+++ src/components/projetos/index.tsx
@@ -1,26 +1,30 @@
 import './style.css';
 
-
 export function Projetos() {
 
     return (
-        <div>
-            <article className="card">
-                <div className="circle"></div>
-                <div className="circle"></div>
-                <div className="circle"></div>
-                <div className="circle"></div>
-                <div className="card-inner">
-                    <p>
+        <article className="card">
+            <div className="circle"></div>
+            <div className="circle"></div>
+            <div className="circle"></div>
+            <div className="circle"></div>
+            <div className="card-inner">
+
+                <div className='imgProjects'>
+                    <img src="\imagens\teste_ft.png" alt="" />
+                </div>
+                <div>
+                    <p className='textProject'>
                         Tenho 18 anos, sou pró-ativo assíduo, sempre motivado para o trabalho em equipe, tenho respeito, franqueza e busco sempre o conhecimento e aprimoramento técnico/profissional e de outros assuntos de meu interesse cultural, tenho um bom relacionamento em geral... Além da programação, tenho o UI/UX designer como hobby, já entreguei diversos produtos (Publicações, logos, promoções, posts de engajamento,...) desta área para clientes.
                         Já li livros como: O Poder, Mais Esperto Que o Diabo, Pai Rico Pai Pobre. Atualmente estou lendo O Poder do Hábito.
                         Tenho 18 anos, sou pró-ativo assíduo, sempre motivado para o trabalho em equipe, tenho respeito, franqueza e busco sempre o conhecimento e aprimoramento técnico/profissional e de outros assuntos de meu interesse cultural, tenho um bom relacionamento em geral... Além da programação, tenho o UI/UX designer como hobby, já entreguei diversos produtos (Publicações, logos, promoções, posts de engajamento,...) desta área para clientes.
                         Já li livros como: O Poder, Mais Esperto Que o Diabo, Pai Rico Pai Pobre. Atualmente estou lendo O Poder do Hábito.
                     </p>
+                    <button className='gitHub'><img src="\imagens\icons8-github.svg" alt="" />Git Hub</button>
                 </div>
-            </article>
-        </div>
+
+            </div>
+        </article>
     )
 }
-
 
